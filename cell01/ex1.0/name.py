01709491ceef2e44b06625fd0b0be016d6e1685b
P-NAY <phongsakorn.en@gmail.com>
2025-06-16 09:03:18 +0000

    new

---
+++ cell01/ex1.0/name.py
@@ -0,0 +1,3 @@
+first_name = "phogsakorn"
+Last_name = "ratsena"
+print(first_name,Last_name)
---
+++ cell01/ex1.3/whatsyourname.py
@@ -0,0 +1,4 @@
+first_name = input("phongsakorn: ")
+last_name = input("ratsena: ")
+
+print(first_name, last_name)
