b6d0f240f264f27f0775508eb999ef5cfbc45d58
P-NAY <phongsakorn.en@gmail.com>
2025-06-17 07:01:59 +0000

    new2

--- cell01/ex1.1/name.py
+++ cell01/ex1.1/name.py
@@ -1,4 +1,4 @@
-first_name = "phongsakorn"
-Last_name = "ratsena"
-whole_name = first_name+Last_name
+first_name = "Phongsakorn"
+Last_name = "Ratsena"
+whole_name = first_name + " " + Last_name
 print(whole_name)
