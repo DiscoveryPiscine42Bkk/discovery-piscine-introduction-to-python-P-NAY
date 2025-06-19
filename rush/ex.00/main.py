99f544282c3da8a2cb0d56eba5c0d9edc70b6ed8
P-NAY <phongsakorn.en@gmail.com>
2025-06-19 18:59:00 +0000

    new8.1

---
+++ rush/ex.00/main.py
@@ -0,0 +1,16 @@
+'''Main file - includes both Success and Fail cases'''
+from checkmate import checkmate
+
+def main():
+    print("=== Case 1: King is in check (Expect: Success) ===")
+    board1 = """\
+....
+.K..
+....
+.R..\
+"""
+    checkmate(board1)
+
+if __name__ == "__main__":
+    main()
+
